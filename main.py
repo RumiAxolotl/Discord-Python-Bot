0fd8e9b6dde81c31498f649de1a795451edd2faf
Danny <thanhan1100@gmail.com>
2022-11-02 16:05:41 +0700

    Add Ping.py and Updated main.py

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ with open('./config.json', 'r') as configFile:
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix='*', intents=intents)
 
-
+#Console...
 @client.event
 async def on_ready():
     print(f'{client.user} is ready')
+    channel = client.get_channel(1036297105666490371)
+    #say in chat...
+    await channel.send(f"{client.user} \nNow Online!")
 
-        
 
+
+
+#Bot Token...
 client.run(config["token"])
